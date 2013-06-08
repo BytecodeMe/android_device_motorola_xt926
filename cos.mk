9e49610759ba2fa280d5dd7fbd845ec0187d8aeb
Andrew Warfield <hiasant@gmail.com>
2013-06-08 03:03:13 +0200

    Add ChameleonOS .mk

---
+++ cos.mk
@@ -0,0 +1,25 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cos/config/cdma.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Release name
+PRODUCT_RELEASE_NAME := DROID RAZR HD
+PRODUCT_NAME := cos_xt926
+
+$(call inherit-product, device/motorola/xt926/full_xt926.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_BRAND=motorola \
+    PRODUCT_NAME=XT926_verizon \
+    BUILD_PRODUCT=vanquish \
+    BUILD_FINGERPRINT=motorola/XT926_verizon/vanquish:4.1.2/9.8.1Q-62_VQW_MR-2/$
+
